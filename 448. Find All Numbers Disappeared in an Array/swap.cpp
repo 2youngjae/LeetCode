27e3d05f8e047e536a6e74e95905a540d1d45a50
youngjae <dldudwo10@naver.com>
2021-01-28 21:55:38 +0900

    .

--- 448. Find All Numbers Disappeared in an Array/swap.cpp
+++ 448. Find All Numbers Disappeared in an Array/swap.cpp
@@ -1,3 +1,17 @@
+/**
+ * @file swap.cpp
+ * @author 2YoungJae (dldudwo10@naver.com)
+ * @brief 
+ * @version 0.1
+ * @date 2021-01-28
+ * 
+ * Runtime: 80 ms, faster than 80.25% of C++ online submissions for Find All Numbers Disappeared in an Array.
+ * Memory Usage: 33.6 MB, less than 97.28% of C++ online submissions for Find All Numbers Disappeared in an Array.
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+
 #include <stdio.h>
 #include <vector>
 using namespace std;
@@ -23,24 +37,14 @@ public:
                 if(nums[i] == i + 1 || nums[i] == nums[next]) break;
                 swap(nums, i, next);
                 next = nums[i] - 1;
-                printAll(nums);
             }
         }
 
-        for(int i = 0 ; i < nums.size(); i++){
+        for(int i = 0 ; i < nums.size(); ++i){
             if(nums[i] != i + 1) answer.push_back(i + 1);
         }
 
-        printAll(answer);
-
         return answer;
-    }
-
-    void printAll(vector<int>& v){
-        for(int i = 0 ; i < v.size();i++){
-            printf("%d ", v[i]);
-        }
-        printf("\n");
     }
 };
 
