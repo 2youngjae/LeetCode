2e75a7731ebd8600e6b5448659059af84c88c726
youngjae <dldudwo10@naver.com>
2021-02-02 20:00:16 +0900

    .

--- 85. Maximal Rectangle/bruteforce.cpp
+++ 85. Maximal Rectangle/bruteforce.cpp
@@ -1,3 +1,24 @@
+/**
+ * @file bruteforce.cpp
+ * @author 2YoungJae (dldudwo10@naver.com)
+ * @brief 
+ * @version 0.1
+ * @date 2021-02-01
+ * @copyright Copyright (c) 2021
+ * Runtime: 88 ms, faster than 13.51% of C++ online submissions for Maximal Rectangle.
+ * Memory Usage: 10.8 MB, less than 98.71% of C++ online submissions for Maximal Rectangle.
+ * 
+ * Constraints:
+ * rows == matrix.length
+ * cols == matrix.length
+ * 0 <= rows, cols <= 200
+ * matrix[i][j] is '0' or '1'.
+ * 
+ * Time complexity : O(rows^2 * cols^2) - 2차원 배열의 모든 원소에서 각각 rows*cols번 계산 하므로 시간복잡도는 O((rows*cols) * (rows*cols))
+ * Space complexity : O(rows * cols) - 주어진 matrix의 공간만 사용하므로 O(rows * cols)
+ * 
+ */
+
 #include <iostream>
 #include <vector>
 #include <cmath>
@@ -10,7 +31,6 @@ public:
     {
         int answer = 0;
         int cMax = -1;
-        printf("r = %d, c = %d matrix[r][c] = %d\n", r, c, matrix[r][c]-'0');
         for (int i = r; i < matrix.size(); i++)
         {
             if (matrix[i][c] == '0')
@@ -26,7 +46,6 @@ public:
                 }
                 int rect = (i - r + 1) * (j - c + 1);
                 answer = max(answer, rect);
-                printf("i = %d, j = %d, rect = %d, cMax = %d\n", i, j, rect, cMax);
             }
             if (cMax == -1)
                 cMax = j;
